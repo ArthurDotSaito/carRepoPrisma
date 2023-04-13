ce87463b490d3972832b342e409fef85e030a9f0
Arthur Saito <arthur_saito1998@hotmail.com>
2023-04-13 00:27:34 -0300

    refact: moving db to prisma

--- src/repository/carRepository.ts
+++ src/repository/carRepository.ts
@@ -11,20 +11,30 @@ async function getCar(id: number) {
 }
 
 async function getCarWithLicensePlate(licensePlate: string) {
-  const data = await db.query(`SELECT * FROM cars WHERE "licensePlate" = $1`, [licensePlate]);
-  return data.rows[0];
+  return prisma.cars.findFirst({
+    where:{
+      licensePlate
+    }
+  })
 }
 
 async function createCar(model: string, licensePlate: string, year: number, color: string) {
-  await db.query(
-    `INSERT INTO cars (model, "licensePlate", year, color)
-     VALUES ($1, $2, $3, $4)`,
-    [model, licensePlate, year, color]
-  );
+  return prisma.cars.create({
+    data:{
+      model,
+      licensePlate,
+      year,
+      color
+    }
+  })
 }
 
 async function deleteCar(id: number) {
-  await db.query(`DELETE FROM cars WHERE id = $1`, [id]);
+  return prisma.cars.delete({
+    where:{
+      id,
+    }
+  })
 }
 
 const carRepository = {
